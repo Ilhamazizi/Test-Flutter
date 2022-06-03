3cf5af0d3211ec3d379e0ac03e9a503c7e5a7bdd
Ilhamazizi <muh.ilhamazizi@gmail.com>
2022-06-04 06:52:13 +0700

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/azeezee/Documents/Test-Flutter
+# Build directory: /home/azeezee/Documents/Test-Flutter/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
